23c82083fc397e98b2721542216c2e5ab9fe40d8
zhuzhenke <zhenke.zhu@lazada.com>
2018-11-24 08:10:54 +0800

    add gif link

--- sorting-algorithms/src/main/java/com/common/algorithms/sortingtest/InsertionSort.java
+++ sorting-algorithms/src/main/java/com/common/algorithms/sortingtest/InsertionSort.java
@@ -4,6 +4,9 @@ import com.common.algorithms.basic.Sort;
 import com.common.algorithms.basic.SortUtils;
 
 /**
+ * document:https://en.wikipedia.org/wiki/Insertion_sort
+ * gif demo:https://upload.wikimedia.org/wikipedia/commons/0/0f/Insertion-sort-example-300px.gif
+ *
  * @author zhuzhenke
  * @date 2018/11/24
  */
@@ -13,12 +16,11 @@ public class InsertionSort implements Sort {
     public int[] sort(int[] sortingData) {
         SortUtils.check(sortingData);
 
-
         for (int i = 1; i < sortingData.length; i++) {
             int currentValue = sortingData[i];
             int j = i;
             while (j - 1 >= 0 && currentValue < sortingData[j - 1]) {
-                sortingData[j] = sortingData[j-1];
+                sortingData[j] = sortingData[j - 1];
                 j--;
             }
             sortingData[j] = currentValue;
