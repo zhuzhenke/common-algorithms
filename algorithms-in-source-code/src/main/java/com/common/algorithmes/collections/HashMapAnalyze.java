2e19997e8df27d614b6197dd8b84dbf21c0c387f
zhuzhenke <zhenke.zhu@lazada.com>
2018-12-06 09:06:13 +0800

    HashMapAnalyze

---
+++ algorithms-in-source-code/src/main/java/com/common/algorithmes/collections/HashMapAnalyze.java
@@ -0,0 +1,69 @@
+package com.common.algorithmes.collections;
+
+import java.util.HashMap;
+
+/**
+ * @author zhuzhenke
+ * @date 2018/12/6
+ */
+public class HashMapAnalyze {
+    public static void main(String[] args) {
+        initTableSize();
+    }
+
+    /**
+     * algorithm of initialCapacity
+     * how to Returns a power of two size as table length for the given target capacity.
+     *
+     * @since jdk 1.8
+     */
+    private static void initTableSize() {
+        //this constructor will init HashMap's table size according to initialCapacity param
+        //it would invoke java.util.HashMap.tableSizeFor
+        HashMap<String, String> hashMap = new HashMap<>(13);
+
+        int tableSize = tableSizeFor(16);
+        System.out.println(tableSize);
+    }
+
+    static final int MAXIMUM_CAPACITY = 1 << 30;
+
+    /**
+     * Returns a power of two size for the given target capacity.
+     */
+    static final int tableSizeFor(int cap) {
+        //cap:10000
+        //n:1111
+        int n = cap - 1;
+        //n >>> 1:111   1111|111:1111
+        n |= n >>> 1;
+        //n >>> 2:11    1111|10:1111
+        n |= n >>> 2;
+        //n >>> 4:0     1111|0:1111
+        n |= n >>> 4;
+        //n >>> 8:0     1111|0:1111
+        n |= n >>> 8;
+        //n >>> 16:0    1111|0:1111
+        n |= n >>> 16;
+        //return 16
+        return (n < 0) ? 1 : (n >= MAXIMUM_CAPACITY) ? MAXIMUM_CAPACITY : n + 1;
+    }
+
+    static final int tableSizeFor13(int cap) {
+        //cap:1101
+        //n:1100
+        int n = cap - 1;
+        //n >>> 1:110   1100|110:1110
+        n |= n >>> 1;
+        //n >>> 2:10    1110|11:1111
+        n |= n >>> 2;
+        //n >>> 4:0     1111|0:1111
+        n |= n >>> 4;
+        //n >>> 8:0     1111|0:1111
+        n |= n >>> 8;
+        //n >>> 16:0    1111|0:1111
+        n |= n >>> 16;
+        //return 16
+        return (n < 0) ? 1 : (n >= MAXIMUM_CAPACITY) ? MAXIMUM_CAPACITY : n + 1;
+    }
+}
